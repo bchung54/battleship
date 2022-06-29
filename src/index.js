41160196bdf704c853720215029c3c40476602a1
bchung-desktop <brianchung54@gmail.com>
2022-06-29 16:14:23 -0700

    Correct css import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import domManager from './domManager';
 import game from './game';
-import './reset.css';
-import './style.css';
+import '../src/reset.css';
+import '../src/style.css';
 
 
 const nameInput = document.querySelector('#player-name');
